e1b11c5b1581a58308913e4b8b7097fe971ea62e
ChrisCheng <gemini2015@hotmail.com>
2014-04-22 14:51:45 +0800

    fix regfile if/else match

--- regfile.v
+++ regfile.v
@@ -22,62 +22,37 @@
 *	writeEnable: RegWrite
 *
 */
-module RegFile(clk, rst, regA, regB, regW, Wdat, Adat, Bdat, RegWrite, regC, Cdat);
+module RegFile(clk, rst, regA, regB, regW, Wdat, Adat, Bdat, RegWrite);
 	
 	input clk, rst, RegWrite;
-   	input [4:0] regA, regB, regW, regC;
+   	input [4:0] regA, regB, regW;
    	input [31:0] Wdat;
-   	output [31:0] Adat, Bdat, Cdat;
+   	output [31:0] Adat, Bdat;
 
    	reg [31:0] iRegf[31:0];
- 
+   	interger i;
+
 	assign Adat = iRegf[regA];
 	assign Bdat = iRegf[regB];
-	assign Cdat = iRegf[regC];
 	
 	always @(posedge clk or posedge rst)
 	begin
 		if(rst)
 		begin
-		// reset reg file
-			iRegf[ 0] <= 0;
-			iRegf[ 1] <= 1;
-			iRegf[ 2] <= 2;
-			iRegf[ 3] <= 3;
-			iRegf[ 4] <= 4;
-			iRegf[ 5] <= 5;
-			iRegf[ 6] <= 6;
-			iRegf[ 7] <= 7;
-			iRegf[ 8] <= 8;
-			iRegf[ 9] <= 9;
-			iRegf[10] <= 10;
-			iRegf[11] <= 11;
-			iRegf[12] <= 12;
-			iRegf[13] <= 13;
-			iRegf[14] <= 14;
-			iRegf[15] <= 15;
-			iRegf[16] <= 16;
-			iRegf[17] <= 17;
-			iRegf[18] <= 18;
-			iRegf[19] <= 19;
-			iRegf[20] <= 20;
-			iRegf[21] <= 21;
-			iRegf[22] <= 22;
-			iRegf[23] <= 23;
-			iRegf[24] <= 24;
-			iRegf[25] <= 25;
-			iRegf[26] <= 26;
-			iRegf[27] <= 27;
-			iRegf[28] <= 28;
-			iRegf[29] <= 29;
-			iRegf[30] <= 30;
-			iRegf[31] <= 31;
+			// reset reg file
+			for (i=1; i<32; i=i+1) begin
+                iRegf[i] <= 0;
+            end
 		end
 		else if(RegWrite)
 		begin
 			//write reg file
 			// $0 is always 0
 			iRegf[regW] <= (regW == 5'b00000) ? 32'h0 : Wdat;
+		end
+		else
+		begin
+			iRegf[regW] <= iRegf[regW];
 		end
 	end
 endmodule
@@ -89,8 +64,14 @@ module Register(clk, rst, RegWrite, idat, odat);
 	output[31:0]	odat;
 	reg[31:0]		odat;
 
+	initial
+		odat = 0;
+
 	always @(posedge clk) begin
-		if(rst)odat<=0;
-		else if(RegWrite) odat <= idat;
+		if(rst) odat <= 0;
+		else begin 
+			if(RegWrite) odat <= idat;
+			else odat <= odat;
+		end
 	end
 endmodule
